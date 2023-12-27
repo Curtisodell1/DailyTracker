aa71f26052c4d58b08ebf69069dbb9b005519b72
Curtis <curtisodell1@gmail.com>
2023-12-27 11:11:22 -0700

    commentary added to first part of sudoku board

--- Learnings/SudokuSolver.py/SudokuPuzzle.py
+++ Learnings/SudokuSolver.py/SudokuPuzzle.py
@@ -22,9 +22,9 @@ def solve(bo):
     else:
         row, col = find
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+    for r in range(1,10):
+        if valid(bo, r, (row, col)):
+            bo[row][col] = r
 
             if solve(bo):
                 return True
@@ -36,51 +36,57 @@ def solve(bo):
 
 def valid(bo, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for r in range(len(bo[0])):
+        if bo[pos[0]][r] == num and pos[1] != r:
             return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for c in range(len(bo)):
+        if bo[c][pos[1]] == num and pos[0] != c:
             return False
 
     # Check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    for r in range(box_y*3, box_y*3 + 3):
+        for c in range(box_x * 3, box_x*3 + 3):
+            if bo[r][c] == num and (r,c) != pos:
                 return False
 
     return True
 
-
+#Here is our function to print the board in the CLI
 def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
-
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
+    #r is set to define our rows in the range of the provided board (typically 9)
+    for r in range(len(bo)):
+        #if r is a multiple of 3 and not 0 we add a line inbetween the rows
+        if r % 3 == 0 and r != 0:
+            print("- - - - - - - - - - - - ")
+        #if the column is a multiple of 3 and not 0 we add a verticle line 
+            #we are looking at the board's row using bo[0]
+            #end="" is used to prevent us from moving to a new line after performing this action
+        for c in range(len(bo[0])):
+            if c % 3 == 0 and c != 0:
                 print(" | ", end="")
-
-            if j == 8:
-                print(bo[i][j])
+            #if we are on the last column (8) of the row we just print that value.
+            if c == 8:
+                print(bo[r][c])
+            #lastly, we print the number if it isn't one of the above conditions and a space behind it to add a little visual relief
+                # the end="" again prevents the new line from being put in.
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(bo[r][c]) + " ", end="")
 
 
 def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
+    for r in range(len(bo)):
+        for c in range(len(bo[0])):
+            if bo[r][c] == 0:
+                return (r, c)  # row, col
 
     return None
 
 print_board(board)
 solve(board)
-print("___________________")
+print("_______________________")
 print_board(board)
